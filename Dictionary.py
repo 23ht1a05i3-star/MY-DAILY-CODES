37a29218fb8a8aca8eaff2dc640ebf7592b0cd16
Dinesh <23ht1a05i3@gmail.com>
2026-08-05 22:00:37 +0530

    my update scenario dictionary codes

--- Dictionary.py
+++ Dictionary.py
@@ -59,3 +59,85 @@ student = {
 
 for key, value in student.items():
     print(key, ":", value)
+
+#Real world scenario questions
+#Student Management System
+students = {}
+
+name = input("Enter Student Name: ")
+marks = int(input("Enter Marks: "))
+students[name] = marks
+print(students)
+
+#ATM Balance Check
+accounts = {
+    "1001": 5000,
+    "1002": 8000
+}
+acc = input("Enter Account Number: ")
+if acc in accounts:
+    print("Balance =", accounts[acc])
+else:
+    print("Account Not Found")
+
+#Shopping Cart
+cart = {
+    "Rice": 60,
+    "Sugar": 45,
+    "Oil": 150
+}
+
+print("Items:", cart)
+item = input("Enter Item: ")
+if item in cart:
+    print("Price =", cart[item])
+else:
+    print("Item Not Available")
+
+#Hospital Patient Record
+patients = {
+    "101": "Fever",
+    "102": "Diabetes"
+}
+pid = input("Patient ID: ")
+if pid in patients:
+    print("Disease:", patients[pid])
+else:
+    print("Patient Not Found")
+
+#Country and Capital
+country = {
+    "India": "New Delhi",
+    "Japan": "Tokyo",
+    "USA": "Washington D.C."
+}
+name = input("Country: ")
+print(country.get(name, "Country Not Found"))
+
+#Product Price Checker
+products = {
+    "Laptop": 50000,
+    "Mouse": 500,
+    "Keyboard": 1000
+}
+product = input("Product Name: ")
+if product in products:
+    print("Price =", products[product])
+else:
+    print("Product Not Found")
+
+#Voting EligibilityVoting Eligibility
+voters = {
+    "Rahul": 21,
+    "Anil": 16
+}
+
+name = input("Enter Name: ")
+
+if name in voters:
+    if voters[name] >= 18:
+        print("Eligible to Vote")
+    else:
+        print("Not Eligible")
+else:
+    print("Person Not Found")
