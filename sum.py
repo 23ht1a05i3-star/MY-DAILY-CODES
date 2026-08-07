c7a7bbf25d054a38a9bf5bca18aa0532dfeafd07
Dinesh <23ht1a05i3@gmail.com>
2026-08-07 20:08:20 +0530

    my sum codes

--- sum.py
+++ sum.py
@@ -1,32 +1,22 @@
 #adding two float numbers with user given inputs
 a = float(input("Enter first number: "))
 b = float(input("Enter second number: "))
-
 sum = a + b
-
 print("Sum =", sum)
 
 #adiing two integer numbers with user inputs
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-
 sum = a + b
-
 print("Sum =", sum)
-
 
 #Shopping Bill Calculator
 price1 = float(input("Enter price of Item 1: "))
 price2 = float(input("Enter price of Item 2: "))
-
 total_bill = price1 + price2
-
 print("Total Bill = ₹", total_bill)
-
 #Student Total Marks
 math = int(input("Enter Math marks: "))
 science = int(input("Enter Science marks: "))
-
 total = math + science
-
 print("Total Marks =", total)
