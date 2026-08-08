a604a7c3879f744c95c27a39902bb832b4faa68f
Dinesh <23ht1a05i3@gmail.com>
2026-08-08 19:15:38 +0530

    my thread main code

--- Heart_of_Thread.py
+++ Heart_of_Thread.py
@@ -96,3 +96,102 @@ lock.acquire()
 lock.release()
 
 '''
+import threading
+count = 0
+lock = threading.Lock()
+
+def increment():
+    global count
+    for i in range(10000):
+        with lock:
+        #critical section
+            count +=1
+        
+t1 = threading.Thread(target=increment)
+t2 = threading.Thread(target=increment)
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+print(count)
+
+#Bank example:
+class Bank:
+    def __init__(self):
+        self.balance = 1000
+
+    def withdraw(self,amount):
+        if self.balance >= amount:
+            self.balance -= amount
+
+#T1 = 500
+#T2 = 700          #negative
+
+import threading
+class Bank:
+    def __init__(self):
+        self.balance  = 1000
+        self.lock = threading.Lock()
+    def withdraw(self,amount):
+        with lock:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(amount,"withdraw")
+            else:
+                print("Insufficient Balance")
+bank = Bank()
+t1 = threading.Thread(
+    target=bank.withdraw,
+    args= (700,)
+)
+t2 = threading.Thread(
+    target=bank.withdraw,
+    args= (500,)
+)
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+print(bank.balance)
+
+'''
+Deadlock:
+Where the threds wait forever for locks
+Thread 1
+Lock A
+waiting for Lock B
+
+Thread 2
+Lock B
+waiting for Lock A
+
+Thread 1 --> Lock A
+Thread 2 --> Lock B
+deadlock
+
+
+Rlock:
+a thread can acquire same lock multiple times
+
+whyRlock/;
+normal lock
+acquire lock
+release lock
+
+if same lock acquire again dead lock
+'''
+# import threading
+# lock=threading.Lock()
+# def outer():
+#     lock.acquire()
+#     inner()
+#     lock.release()
+# def inner():
+#     lock.acquire()
+#     print("inner")
+#     lock.release()
+# outer()
