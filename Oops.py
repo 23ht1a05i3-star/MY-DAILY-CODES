6a9783bba5327309fa99140a3bfcc92f33c04a5a
Dinesh <23ht1a05i3@gmail.com>
2026-08-09 21:28:07 +0530

    my encapsulationa nd inheritence  concepts inoops

--- Oops.py
+++ Oops.py
@@ -53,3 +53,69 @@ class Student:
         print("Marks:", self.marks)
 student = Student("Narendra", 92)
 student.display()
+
+#Encapsulation-->used for encapsulation and for security
+
+#Encapsulation means combining data and methods 
+# inside a class and controlling access to internal data.
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance
+    def deposit(self, amount):
+        if amount > 0:
+            self.__balance += amount
+    def show_balance(self):
+        print("Balance:", self.__balance)
+account = BankAccount(5000)
+account.deposit(2000)
+account.show_balance()
+
+#Inheritance
+#Inheritance allows one class
+#  to acquire properties and methods from another class.
+
+class Animal:
+    def eat(self):
+        print("Animal is eating")
+class Dog(Animal):
+    def bark(self):
+        print("Dog is barking")
+dog = Dog()
+dog.eat()
+dog.bark()
+
+#Types of Inheritance
+
+# Python supports:
+
+# Single Inheritance
+# Multiple Inheritance
+# Multilevel Inheritance
+# Hierarchical Inheritance
+# Hybrid Inheritance
+
+# Single Inheritance
+class Animal:
+    def eat(self):
+        print("Eating")
+class Dog(Animal):
+    def bark(self):
+        print("Barking")
+dog = Dog()
+dog.eat()
+dog.bark()
+
+#Multilevel Inheritance
+class GrandParent:
+    def house(self):
+        print("Grandparent's house")
+class Parent(GrandParent):
+    def car(self):
+        print("Parent's car")
+class Child(Parent):
+    def bike(self):
+        print("Child's bike")
+child = Child()
+child.house()
+child.car()
+child.bike()
